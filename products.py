3319601e174416e0c87355fc71943c06a11deb20
ming1208 <weng022768@gmail.com>
2020-09-25 13:48:42 +0800

    Write to the .txt file

--- products.py
+++ products.py
@@ -4,6 +4,11 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入產品價錢: ')
+	price = int(price)
 	products.append([name,price])
 print('------------------------------------')
 print(products)
+print('------------------------------------')
+with open('products.txt', 'w') as f:
+	for p in products:
+		f.write(p[0]+ ','+ str(p[1])+'\n')
